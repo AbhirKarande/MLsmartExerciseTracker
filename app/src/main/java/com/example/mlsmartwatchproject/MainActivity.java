8990717b63d7e9448f7e7d33d6e63622f1b6dfef
AbhirHI <abhir511@gmail.com>
2022-04-17 01:40:09 -0400

    got sensor listening to work, not need to find a way to store the measurements

--- app/src/main/java/com/example/mlsmartwatchproject/MainActivity.java
+++ app/src/main/java/com/example/mlsmartwatchproject/MainActivity.java
@@ -1,28 +1,42 @@
 package com.example.mlsmartwatchproject;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.wearable.view.DotsPageIndicator;
 import android.support.wearable.view.GridViewPager;
 import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener {
+    private static final String TAG = "MainActivity";
+    private SensorManager sensorManager;
+    Sensor accelerometer;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: Initializing Sensor Services");
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        Log.d(TAG, "onCreate: Registered accelerometer listener");
 
-        WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override public void onLayoutInflated(WatchViewStub stub) {
-                final GridViewPager pager = (GridViewPager) findViewById(R.id.pager);
-                pager.setAdapter(new SensorFragmentPagerAdapter(getFragmentManager()));
+    }
 
-                DotsPageIndicator indicator = (DotsPageIndicator) findViewById(R.id.page_indicator);
-                indicator.setPager(pager);
-            }
-        });
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Log.d(TAG, "onSensorChanged: X: " + sensorEvent.values[0] + "Y: " + sensorEvent.values[1] + "Z: " + sensorEvent.values[2]);
+
     }
 }
