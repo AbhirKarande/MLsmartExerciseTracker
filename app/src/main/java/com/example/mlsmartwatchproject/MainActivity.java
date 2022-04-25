ea0b1a7a04aac384fcfc0347d4280968d9da94a2
AbhirHI <abhir511@gmail.com>
2022-04-25 17:27:14 -0400

    tested out weka with watch

--- app/src/main/java/com/example/mlsmartwatchproject/MainActivity.java
+++ app/src/main/java/com/example/mlsmartwatchproject/MainActivity.java
@@ -21,6 +21,7 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.ActivityRecognition;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
@@ -41,7 +42,7 @@ public class MainActivity extends Activity implements SensorEventListener, Googl
 //    ArrayList<Float> x = new ArrayList<Float>();
     private final int window_size = 4;
     private final int features_selected = 4;
-    private float[] x = new float[window_size*3000];
+    private float[] x = new float[window_size*300];
     private int index = 0;
     private boolean full = false;
     private String[][] data = new String[2][features_selected+1];
@@ -61,7 +62,7 @@ public class MainActivity extends Activity implements SensorEventListener, Googl
         Log.d(TAG, "onCreate: Initializing Sensor Services");
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(MainActivity.this, accelerometer, 1000);
+        sensorManager.registerListener(MainActivity.this, accelerometer, 10000);
 
         try {
             cls = (Classifier) read(getAssets().open("J48ExcerciseRecognition.model"));
@@ -111,33 +112,48 @@ public class MainActivity extends Activity implements SensorEventListener, Googl
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        Log.d(TAG, "onSensorChanged: X: " + sensorEvent.values[0] + " Y: " + sensorEvent.values[1] + " Z: " + sensorEvent.values[2]);
+        //Log.d(TAG, "onSensorChanged: X: " + sensorEvent.values[0] + " Y: " + sensorEvent.values[1] + " Z: " + sensorEvent.values[2]);
 //        x.add(sensorEvent.values[0]);
-        if(index == window_size-1)
+        if(index == window_size*100-1)
         	full = true;
         x[index*3] = sensorEvent.values[0];
         x[index*3+1] = sensorEvent.values[1];
         x[index*3+2] = sensorEvent.values[2];
-        index = (index+1)%(window_size*1000);
-        if(full&&index%1000 == 1) {
+        index = (index+1)%(window_size*100);
+        if(full&&index%100 == 1) {
         	data[1][0] = String.valueOf((x[0]+x[3]+x[6]+x[9])/4);
         	data[1][1] = String.valueOf(findMedian(0));
         	data[1][2] = String.valueOf(findMedian(1));
         	data[1][3] = String.valueOf(findMedian(2));
         	data[1][4] = "?";
-	        String arffData = MyWekaUtils.csvToArff(data, new int[]{0,1,2,3});
-	        StringReader strReader = new StringReader(arffData);
-			Instances unlabeled = new Instances(strReader);
-			unlabeled.setClassIndex(unlabeled.numAttributes() - 1);
-			double clsLabel = cls.classifyInstance(unlabeled.instance(0));
-			Log.d(clsLabel);
+            String arffData = null;
+            try {
+                arffData = MyWekaUtils.csvToArff(data, new int[]{0,1,2,3});
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            StringReader strReader = new StringReader(arffData);
+            Instances unlabeled = null;
+            try {
+                unlabeled = new Instances(strReader);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            unlabeled.setClassIndex(unlabeled.numAttributes() - 1);
+            double clsLabel = 0;
+            try {
+                clsLabel = cls.classifyInstance(unlabeled.instance(0));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            Log.d(TAG, "Classification: " + clsLabel);
         }
     }
     
     private float findMedian(int i) {
 		PriorityQueue<Float> p = new PriorityQueue<>();
-		for(int j = 0; j < 4000; j++) {
-			if(p.size()==2001) {
+		for(int j = 0; j < 400; j++) {
+			if(p.size()==201) {
 				p.poll();
 			}
 			p.add(x[i+j*3]);
--- app/src/main/java/com/example/mlsmartwatchproject/MyWekaUtils.java
+++ app/src/main/java/com/example/mlsmartwatchproject/MyWekaUtils.java
@@ -1,4 +1,4 @@
-
+package com.example.mlsmartwatchproject;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.PrintWriter;
